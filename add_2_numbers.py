d1b357a3c1b04c7650ace411abe5c251d0a01c47
M18Bhoir <159646870+M18Bhoir@users.noreply.github.com>
2025-05-18 00:01:13 +0530

    Add files via upload

---
+++ add_2_numbers.py
@@ -0,0 +1,7 @@
+# In this code we have taken 2 numbers as input and added them in a variable called sum
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
+
+sum = (a + b)
+print(f"The sum is : {sum}")
+
