3d76f4f421c63cb3bac0465bdd36fd415e61bbe5
M18Bhoir <159646870+M18Bhoir@users.noreply.github.com>
2025-05-18 12:06:17 +0530

    Add files via upload

---
+++ area_of_triangle.py
@@ -0,0 +1,8 @@
+# In this code, we have created two variables, height and breadth, and calculated the area of a triangle using the formula (1/2) × breadth × height.
+height = int(input("Enter height : "))
+
+breadth = int(input("Enter breadth : "))
+
+area = int((1/2)*breadth*height)
+
+print(f"The area is : {area}")
