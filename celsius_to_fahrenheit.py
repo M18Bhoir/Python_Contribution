2f89c231fa8414e41a463bb51f7265733a2518b9
M18Bhoir <159646870+M18Bhoir@users.noreply.github.com>
2025-05-19 17:06:40 +0530

    Add files via upload

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+#In this code we have taken celsius as input and converted it with the formula with the help of the formula
+celsius = float(input("Enter temperature in celsius : "))
+
+fahrenheit = (celsius * (9/5))+ 32
+
+print(f"The temperature in fahrenheit is : {fahrenheit}")
