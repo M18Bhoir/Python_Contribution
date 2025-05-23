f2fa32b067c0ade0b0dc8a9104af6608bb669d9c
M18Bhoir <159646870+M18Bhoir@users.noreply.github.com>
2025-05-23 12:07:01 +0530

    Add files via upload

---
+++ positive_negative_or_zero.py
@@ -0,0 +1,9 @@
+# In this code we check whether the number is positive negative or zero
+n = int(input("Enter a number:"))
+
+if(n > 0): # number is greater than zero
+    print("The number is positive ")
+elif(n < 0): # number is smaller than zero
+    print("The number is negative")
+else: # numbber is equal to zero
+    print("The number is zero")
