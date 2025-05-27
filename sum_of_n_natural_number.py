5ad73e5d948e12a53ced0f809f2a56db15da63f1
M18Bhoir <159646870+M18Bhoir@users.noreply.github.com>
2025-05-27 15:30:17 +0530

    Add files via upload

--- sum_of_n_natural_number.py
+++ sum_of_n_natural_number.py
@@ -1,5 +1,6 @@
-n = int(input("Enter a number : "))
-sum = 0
-for i in range(1,n+1):
-    sum = i + sum
-print(f"The sum of n natural numbers is : {sum}")
+# In this we are solving a question called sum of n natural numbers
+n = int(input("Enter a number : ")) # Input 
+sum = 0 # initializing and declaring the sum as 0
+for i in range(1,n+1): # for loop for iterating the number
+    sum = i + sum # getting the sum
+print(f"The sum of n natural numbers is : {sum}") 
